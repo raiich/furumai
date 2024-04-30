c03d8378554c8ade3bc51d53cd0da7317fbd9846
raiich <upgrade.outdated@gmail.com>
2024-04-30 22:15:17 +0900

    wip

--- tasks/gensvg-all.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-find ./examples -type f -name \*.furumai -print0 \
-| xargs -0 -I{} sh ./tools/gensvg.sh {}
--- tasks/txt2svg-all.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-find ./examples -type f -name \*.furumai -print0 \
-| xargs -0 -I{} sh -c 'node ./tools/txt2svg.js < {} > ./docs/svg/{}.txt.svg'
